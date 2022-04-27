763ed3e93db86c02cce089cb3375f20b873d1029
Gibril <gibrilla766@gmail.com>
2022-04-27 11:16:45 -0700

    my alx programm

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,27 +2,34 @@
 #include <time.h>
 #include <stdio.h>
 
-/**
-* main - Entry poimt
-*
-* Return: Always 0 (success/correct)
-*/
-int main(void)
-]
-int n;
-srand(time(0));
-n = rand() - RAND_max / 2;
-if (n==0)
-]
-printf("%i is zero/n", n);
-]
+**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successs/correct)
+ */
 
-else if (n<0)
-[printf("5 is negative/n", n);
-]
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n == 0)
+{
+printf("%i is zero\n", n);
+}
+
+else if (n < 0)
+{
+printf("%i is negative\n", n);
+}
+
 else
-[
-printf("%i is positive/n", n);
-]
-return (o);
-]
+{
+printf("%i is positive\n", n);
+}
+
+return (0);
+
+}

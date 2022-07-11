5b96299bddaab25e665337ff1a8d5c4a476236b9
Gibril <gibrilla766@gmail.com>
2022-07-11 08:14:05 -0700

    my loop task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,18 +2,19 @@
 /**
  * _isupper - evalue if letter is uppercase .
  *@c: print int
- * Return: Always 0.
+ *Return: Always 0.
  */
 
  int _isupper(int c)
-{
+ {
  if (c >= 'A' && c <= 'Z')
  {
  return (1);
  }
- 
+
  else
  {
  return (0);
+ }
 
  }
